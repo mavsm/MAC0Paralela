0a217a06f024de69b4d6763039de60bd3d31472f
mavsm <37675051+mavsm@users.noreply.github.com>
2018-03-31 18:20:45 -0300

    ideia basica pro desafio

---
+++ desafio2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+
+int THREAD_NUM = 10;
+
+void thrd(){
+	int i, vetor[100];
+	for(i=0; i<100; i++)
+		vetor[i] = i;
+	pthread_exit(NULL);
+}
+
+int main() {
+	int i;
+	pthread_t *threads;
+
+	threads = malloc(THREAD_NUM*sizeof(pthread_t*));
+
+	for(i=0; i<THREAD_NUM; i++)
+		pthread_create(&threads[i], NULL, thrd, NULL);
+
+	for(i=0; i<THREAD_NUM; i++)
+		pthread_join(threads[i], NULL);
+
+	return 0;
+}
